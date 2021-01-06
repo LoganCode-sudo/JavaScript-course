1369a560c107c86cab7a7e28b4929f82c85e6898
Logan <logancoghill18@gmail.com>
2021-01-06 12:01:18 +0200

    finished project
    i found the for and while loop were the easiest to understand

--- JavaScript-Project-02-Roman-Numeral-Converter.js
+++ JavaScript-Project-02-Roman-Numeral-Converter.js
@@ -1,12 +1,20 @@
 //Convert the given number into a roman numeral.
 function convertToRoman(num) {
-    
- var arrNums = [1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100,200,300,400,500,600,700,800,900,1000,];
+//variables for the numbers,their roman equals and the return string
+    const numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const numeral = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    var reRoman ='';
 
- var arrRoman = ["I","II","III","IV","V","VI","VII","VIII","IX","X","XX","XXX","XL","L","LXX","LXXX","XC","C","CC","CCC","CD","D","DC","DCC","DCCC","CM","M",];
-
- var converted ="";
-
-
+//look through the numbers 
+    for(var i =0;i<numbers.length;i++){
+        //find the index that has the closest value
+        while(numbers[i] <= num){
+            //add the roman equal to the return str
+            reRoman += numeral[i];
+            //substract the index value from the test value
+            num-=numbers[i];
+        }
+    }
+    return reRoman;
 }
 console.log(convertToRoman(36));
