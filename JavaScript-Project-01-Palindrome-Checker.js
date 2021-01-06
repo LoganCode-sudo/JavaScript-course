2487dc8a8ecbe546933c2dc88255697fd6da10af
Logan <logancoghill18@gmail.com>
2021-01-06 10:32:14 +0200

    finished project
    ran into problems when trying to compare the two as arrays, but once cnverted to a string it can be easily compared using ===

--- JavaScript-Project-01-Palindrome-Checker.js
+++ JavaScript-Project-01-Palindrome-Checker.js
@@ -4,10 +4,20 @@
 //ignore punctuation, case, spacing and symbols
 
 function palindrome(str) {
+  //we first clean the str and we change the type to an array
+  var cleanStr = str.toLowerCase().replace(/[\W_]/g, "").split('');
+  //we do the same thing here but reverse the str
+  var reverseStr = str.toLowerCase().replace(/[\W_]/g, "").split('').reverse();
 
-}
+  //we then compare the two, but also turn it back to a string so that we can use the ===
+  if(cleanStr.join(' ') === reverseStr.join(' ')){
+    return true;
+  }else{
+    return false;
+  };
+};
 
-console.log(palindrome("eye"));
-console.log(palindrome("A man, a plan, a canal. Panama"));
-console.log(palindrome("1 eye for of 1 eye."));
+console.log(palindrome("nope"));
+console.log(palindrome("not a palindrome"));
+console.log(palindrome("2A3*3a2"));
 console.log(palindrome("My age is 0, 0 si ega ym."));
