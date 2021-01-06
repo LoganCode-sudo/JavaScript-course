75961ab51e240274b18f6091fe2a9c0f514a786f
Logan <logancoghill18@gmail.com>
2021-01-06 09:34:37 +0200

    Palindrome Checker init
    the function and console.logs decleared

---
+++ JavaScript-Project-01-Palindrome-Checker.js
@@ -0,0 +1,13 @@
+//Palindrome Checker
+//return true if a given string is a palindrome
+//a palindrome is a word or sentence that is spelled the same way both forward and backwards
+//ignore punctuation, case, spacing and symbols
+
+function palindrome(str) {
+
+}
+
+console.log(palindrome("eye"));
+console.log(palindrome("A man, a plan, a canal. Panama"));
+console.log(palindrome("1 eye for of 1 eye."));
+console.log(palindrome("My age is 0, 0 si ega ym."));
