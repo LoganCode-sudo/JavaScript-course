a494eedc7afc02ed682e9e7b3bae509231db019b
Logan <logancoghill18@gmail.com>
2021-01-06 11:15:01 +0200

    fileNames changed and Roman numeral init
    corrected notes, file names gave me OCD. the variables for the next project(roman numerals) was also made

---
+++ JavaScript-Project-02-Roman-Numeral-Converter.js
@@ -0,0 +1,12 @@
+//Convert the given number into a roman numeral.
+function convertToRoman(num) {
+    
+ var arrNums = [1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100,200,300,400,500,600,700,800,900,1000,];
+
+ var arrRoman = ["I","II","III","IV","V","VI","VII","VIII","IX","X","XX","XXX","XL","L","LXX","LXXX","XC","C","CC","CCC","CD","D","DC","DCC","DCCC","CM","M",];
+
+ var converted ="";
+
+
+}
+console.log(convertToRoman(36));
